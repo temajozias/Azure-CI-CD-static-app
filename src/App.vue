77347d616d5366b3be867f67e567be07086e0b8d
TemaJozias <temajozias@gmail.com>
2025-02-28 08:49:30 +0100

    fixed AB#1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
 
     <h1>Tema Gen Ai & AI Agents Platform</h1>
-	<h2>Upload your data to get Gen Ai predictions</h2>
+    <h2>Enjoy the full service</h2>
+	  <h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
