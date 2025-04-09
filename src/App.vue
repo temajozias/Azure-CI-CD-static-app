986750074ed59fc0d60f0dbc2ebf94cb6fa9e6d4
temajozias <temajozias@gmail.com>
2025-04-09 15:36:18 +0100

    fix AB#7

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
     <h2>Wanda Maximoff, The Scarlett Witch</h2>
     <h2>Bucky Barnes, The Winter Soldier</h2>
     <h2>Dr Steven Strange, The Sorcerer Supreme</h2>
+    <h2>Peter Parker, Spiderman</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
