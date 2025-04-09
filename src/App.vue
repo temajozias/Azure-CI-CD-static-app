5dd122dbea8a099a40d280dd54a2f67f50ceec81
temajozias <temajozias@gmail.com>
2025-04-09 13:11:38 +0100

    AB#4

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <h2>Tema Jozias, aspiring MLOps engineer.</h2>
     <h2>Tony Stark, Iron Man with the Mark 42 Bleeding Edge</h2>
     <h2>Tony Stark, Iron Man with the Mark 42 Bleeding Edge</h2>
+    <h2>Col James Rhodes, War Machine</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
